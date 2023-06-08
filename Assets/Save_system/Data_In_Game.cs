4440a3c153d69f2eec0064bb30bc4fae4d33d936
PolkWp <marekvarisella@onet.pl>
2023-06-08 17:20:54 +0200

    Revert "Revert "Revert "Finish Scene, Points system, Start Camera on Shop."""
    This reverts commit 314843c15c53deb55ef3f632b140c61086cf8230.

--- Assets/Save_system/Data_In_Game.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Data_In_Game : MonoBehaviour
-{
-    public int Points;
-    public int Enemy_Killed;
-}
